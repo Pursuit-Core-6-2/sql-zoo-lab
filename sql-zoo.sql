c2248bb35bceff461ad70442347abccbf95dbfe2
Scripter12 <elicardona57@gmail.com>
2019-10-22 15:56:33 -0400

    first commit

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -1,1 +1,35 @@
 -- This is a comment in sql code
+
+--
+-- 0 SELECT basics
+--
+
+-- 1
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+--2
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark')
+
+--3
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+--
+-- 1 SELECT name
+--
+
+-- 1
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+--2
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+--3
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+--4 
