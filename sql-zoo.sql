b98eef1a43c171e74684fda065659b7a68508a14
gsanchez13 <gisellesanchez@pursuit.org>
2019-10-22 20:25:14 -0400

    names questions

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -9,7 +9,55 @@ WHERE name IN ('sweden', 'norway', 'denmark')
 --3
 SELECT name, area FROM world
 WHERE area BETWEEN 200000 AND 250000
+--SELECT name
+--1
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+--2
+SELECT name FROM world
+  WHERE name LIKE '%y'
+--3
+SELECT name FROM world
+  WHERE name LIKE '%x%'
 --4
+SELECT name FROM world
+  WHERE name LIKE '%land'
+--5
+SELECT name FROM world
+  WHERE name LIKE 'c%ia'
+--6
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+--7
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+--8
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+--9
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+--10
+SELECT name FROM world
+ WHERE LENGTH(name) = 4
+--11
+SELECT name
+  FROM world
+ WHERE name LIKE capital
+--12
+SELECT name
+  FROM world
+ WHERE capital = concat(name, ' City')
+--13
+SELECT capital, name FROM world
+WHERE capital LIKE concat(name, '%')
+--14
+SELECT capital, name FROM world
+WHERE capital LIKE concat(name, '%') AND name <> capital
+--15
+SELECT name, REPLACE(capital, name, '') FROM world
+WHERE capital LIKE concat(name, '%') AND name <> capital
 
 --SELECT from WORLD tutorial
 --1
