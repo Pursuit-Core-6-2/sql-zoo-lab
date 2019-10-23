b3fb5c6919357e3eba9e06675e79496bbce33f0a
sergiocohens <sergiocohensalama@pursuit.org>
2019-10-22 21:03:50 -0400

    morejoin

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -222,3 +222,32 @@ HAVING SUM(population) >= 100000000
 --The JOIN operation
 
 --1
+SELECT matchid, player FROM goal
+  WHERE teamid = 'GER'
+
+--2
+SELECT id,stadium,team1,team2
+  FROM game
+ WHERE id=1012
+
+--3
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=goal.matchid)
+  WHERE teamid='GER'
+
+--4
+SELECT team1,team2,player
+  FROM game JOIN goal ON (id=goal.matchid)
+ WHERE player LIKE 'Mario%'
+
+--5
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON (teamid=id)
+ WHERE gtime<=10
+
+--6
+SELECT mdate, teamname
+  FROM game JOIN eteam ON (team1=eteam.id)
+ WHERE coach='Fernando Santos'
+
+--7
