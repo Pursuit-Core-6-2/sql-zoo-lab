ed9e30613fd63abd26b998f1ec7802f4c8e0bc38
sergiocohens <sergiocohensalama@pursuit.org>
2019-10-22 20:20:01 -0400

    more

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -122,3 +122,60 @@ SELECT * FROM nobel
  --10
  SELECT * FROM nobel
  WHERE (yr < 1910 AND subject='Medicine') OR (yr >= 2004 AND subject='Literature')
+
+
+--SELECT from SELECT
+
+--1
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+--2
+SELECT name FROM world
+ WHERE gdp/population >
+      (SELECT gdp/population FROM world WHERE name='United Kingdom')
+   AND continent = 'Europe'
+
+--3
+SELECT name, continent FROM world
+ WHERE continent=
+(SELECT continent FROM world WHERE name = 'Argentina')
+    OR continent=
+(SELECT continent FROM world WHERE name = 'Australia')
+ORDER BY name
+
+--4
+SELECT name, population FROM world
+ WHERE population>
+(SELECT population FROM world WHERE name='Canada')
+   AND population<
+(SELECT population FROM world WHERE name='Poland')
+
+--5
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name='Germany')*100),'%')
+
+  FROM world
+ WHERE continent = 'Europe'
+
+--6
+SELECT name
+  FROM world
+ WHERE gdp > ALL (SELECT gdp FROM world WHERE gdp>0 AND continent='Europe')
+
+--7
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+
+--8
+SELECT continent,name FROM world x
+ WHERE name = (SELECT name FROM world y WHERE x.continent=y.continent LIMIT 1)
+
+
+--SUM and COUNT
+
+--1
