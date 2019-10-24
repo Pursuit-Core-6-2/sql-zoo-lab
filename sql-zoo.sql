e68ed35ddd0f12a4c9ef45fe3c7c70164a640e4d
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-24 03:47:58 -0400

    0.3, tutorial 3 fully completed

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -175,3 +175,89 @@ SELECT name
 --
 
 -- 1
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1950;
+
+-- 2
+SELECT winner
+  FROM nobel
+  WHERE yr = 1962 AND subject = 'Literature';
+
+-- 3
+SELECT yr, subject
+  FROM nobel
+  WHERE winner = 'Albert Einstein';
+
+-- 4
+SELECT winner
+  FROM nobel
+  WHERE yr >= 2000 AND subject = 'Peace';
+
+-- 5
+SELECT *
+  FROM nobel
+  WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+
+-- 6
+SELECT *
+  FROM nobel
+  WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+-- 7
+SELECT winner
+  FROM nobel
+  WHERE winner LIKE 'John_%';
+
+-- 8
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1980 AND subject = 'Physics' XOR subject = 'Chemistry' AND yr = 1984;
+
+-- 9
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine';
+
+-- 10
+SELECT yr, subject, winner
+  FROM nobel
+  WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004;
+
+-- HARD 11
+SELECT *
+  FROM nobel
+  WHERE winner = 'Peter Grünberg';
+
+-- HARD 12
+SELECT *
+  FROM nobel
+  WHERE winner = 'Eugene O''Neill';
+
+-- HARD 13
+SELECT winner, yr, subject
+  FROM nobel
+  WHERE winner LIKE 'Sir_%'
+  ORDER BY yr DESC;
+
+-- HARD 14
+SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY subject IN ('Chemistry', 'Physics') ASC, subject ASC, winner ASC;
+
+
+-- -- -- 4 SELECT within SELECT
+--
+
+-- 1
+-- 2
+-- 3
+-- 4
+-- 5
+-- 6
+-- 7
+-- 8
+-- DIFFICULT 9
+-- DIFFICULT 10
+
