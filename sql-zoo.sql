e9f0bbda3c7ca5da52f7d19304efc5506833c1f7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-26 03:29:08 -0400

    0.6, tutorial 6 fully completed

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -504,15 +504,124 @@ HAVING SUM(population) > 100000000;
 --
 
 -- 1
+SELECT matchid
+   , player
+FROM goal 
+WHERE teamid = 'GER';
+
 -- 2
+SELECT id
+   , stadium
+   , team1
+   , team2
+FROM game
+WHERE id = 1012;
+
 -- 3
+SELECT player
+   , teamid
+   , stadium
+   , mdate
+FROM game
+JOIN goal ON (id = matchid)
+WHERE teamid = 'GER';
+
 -- 4
+SELECT team1
+   , team2
+   , player
+FROM game
+JOIN goal ON (id = matchid)
+WHERE player LIKE 'Mario%';
+
+
 -- 5
+SELECT player
+   , teamid
+   , coach
+   , gtime
+FROM goal
+JOIN eteam ON (teamid = id)
+WHERE gtime <= 10;
+
 -- 6
+SELECT mdate
+   , teamname
+FROM game
+JOIN eteam ON (team1 = eteam.id)
+WHERE coach = 'Fernando Santos';
+
 -- 7
+SELECT player
+FROM game
+JOIN goal ON (matchid = id)
+WHERE stadium = 'National Stadium, Warsaw';
+
 -- HARD 8
+SELECT DISTINCT player
+FROM game
+JOIN goal ON (matchid = id)
+WHERE (
+      team1 = 'GER'
+      OR team2 = 'GER'
+      )
+   AND teamid <> 'GER';
+
 -- HARD 9
+SELECT teamname
+   , COUNT(*)
+FROM eteam
+JOIN goal ON (id = teamid)
+GROUP BY teamname;
+
 -- HARD 10
+SELECT stadium
+   , COUNT(*)
+FROM game
+JOIN goal ON (matchid = id)
+GROUP BY stadium;
+
 -- HARD 11
+SELECT matchid
+   , mdate
+   , COUNT(*)
+FROM game
+JOIN goal ON (matchid = id)
+WHERE team1 = 'POL'
+   OR team2 = 'POL'
+GROUP BY matchid
+   , mdate;
+
 -- HARD 12
+SELECT matchid
+   , mdate
+   , COUNT(*)
+FROM game
+JOIN goal ON (matchid = id)
+WHERE teamid = 'GER'
+GROUP BY matchid
+   , mdate;
+
 -- HARD 13
+SELECT mdate
+   , team1
+   , SUM(CASE 
+         WHEN team1 = teamid
+            THEN 1
+         ELSE 0
+         END) AS score1
+   , team2
+   , SUM(CASE 
+         WHEN team2 = teamid
+            THEN 1
+         ELSE 0
+         END) AS score2
+FROM game
+LEFT OUTER JOIN goal ON (matchid = id)
+GROUP BY mdate
+   , team1
+   , team2
+ORDER BY mdate ASC
+   , matchid ASC
+   , team1 ASC
+   , team2 ASC;
