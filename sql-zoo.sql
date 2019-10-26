a48a275add8638e6701e31d6913a504864ff6a99
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-26 01:04:18 -0400

    0.4, tutorial 4 fully completed

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -1,7 +1,7 @@
--- Joseph P. Pasaoa
--- SQL Zoo Lab
---
---
+/*
+Joseph P. Pasaoa
+SQL Zoo Lab
+*/
 
 
 -- -- -- 0 SELECT basics
@@ -9,18 +9,25 @@
 
 -- 1
 SELECT population
-  FROM world
-  WHERE name = 'Germany';
+FROM world
+WHERE name = 'Germany';
 
 -- 2
-SELECT name, population
-  FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name
+   , population
+FROM world
+WHERE name IN (
+   'Sweden'
+   , 'Norway'
+   , 'Denmark'
+   );
 
 -- 3
-SELECT name, area
-  FROM world
-  WHERE area BETWEEN 200000 AND 250000;
+SELECT name
+   ,area
+FROM world
+WHERE area BETWEEN 200000
+   AND 250000;
 
 
 -- -- -- 1 SELECT name
@@ -28,226 +35,422 @@ SELECT name, area
 
 -- 1
 SELECT name
-  FROM world
-  WHERE name LIKE 'Y%';
+FROM world
+WHERE name LIKE 'Y%';
 
 -- 2
 SELECT name
-  FROM world
-  WHERE name LIKE '%y';
+FROM world
+WHERE name LIKE '%y';
 
 -- 3
 SELECT name
-  FROM world
-  WHERE name LIKE '%x%';
+FROM world
+WHERE name LIKE '%x%';
 
 -- 4
 SELECT name
-  FROM world
-  WHERE name LIKE '%land';
+FROM world
+WHERE name LIKE '%land';
 
 -- 5
 SELECT name
-  FROM world
-  WHERE name LIKE 'C%ia';
+FROM world
+WHERE name LIKE 'C%ia';
 
 -- 6
 SELECT name
-  FROM world
-  WHERE name LIKE '%oo%';
+FROM world
+WHERE name LIKE '%oo%';
 
 -- 7
 SELECT name
-  FROM world
-  WHERE name LIKE '%a%a%a%';
+FROM world
+WHERE name LIKE '%a%a%a%';
 
 -- 8
 SELECT name
-  FROM world
-  WHERE name LIKE '_t%';
+FROM world
+WHERE name LIKE '_t%';
 
 -- 9
 SELECT name
-  FROM world
-  WHERE name LIKE '%o__o%';
+FROM world
+WHERE name LIKE '%o__o%';
 
 -- 10
 SELECT name
-  FROM world
-  WHERE name LIKE '____';
+FROM world
+WHERE name LIKE '____';
 
 -- HARD 11
 SELECT name
-  FROM world
-  WHERE name = capital;
+FROM world
+WHERE name = capital;
 
 -- HARD 12
 SELECT name
-  FROM world
-  WHERE capital LIKE concat(name, '%City');
+FROM world
+WHERE capital LIKE CONCAT (
+   name
+   , '%City'
+   );
 
 -- HARD 13
-SELECT capital, name
-  FROM world
-  WHERE capital LIKE concat('%', name, '%');
+SELECT capital 
+   , name
+FROM world
+WHERE capital LIKE CONCAT (
+   '%'
+   , name
+   , '%'
+   );
 
 -- HARD 14
-SELECT capital, name
-  FROM world
-  WHERE capital LIKE concat(name, '_%');
+SELECT capital
+   , name
+FROM world
+WHERE capital LIKE CONCAT (
+   name
+   , '_%'
+   );
 
 -- HARD 15
-SELECT name, REPLACE (capital, name, '')
-  FROM world
-  WHERE capital LIKE concat(name, '_%');
+SELECT name
+   , REPLACE(capital, name, '')
+FROM world
+WHERE capital LIKE CONCAT (
+      name
+      , '_%'
+      );
 
 
 -- -- -- 2 SELECT from World
 --
 
 -- 1
-SELECT name, continent, population
-  FROM world;
+SELECT name
+   , continent
+   , population
+FROM world;
 
 -- 2
 SELECT name
-  FROM world
-  WHERE population > 200000000;
+FROM world
+WHERE population > 200000000;
 
 -- 3
-SELECT name, GDP / population
-  FROM world
-  WHERE population > 200000000;
+SELECT name
+   , GDP / population
+FROM world
+WHERE population > 200000000;
 
 -- 4
-SELECT name, population / 1000000
-  FROM world
-  WHERE continent = 'South America';
+SELECT name
+   , population / 1000000
+FROM world
+WHERE continent = 'South America';
 
 -- 5
-SELECT name, population
-  FROM world
-  WHERE name IN ('France', 'Germany', 'Italy');
+SELECT name
+   , population
+FROM world
+WHERE name IN (
+   'France'
+   , 'Germany'
+   , 'Italy'
+   );
 
 -- 6
 SELECT name
-  FROM world
-  WHERE name LIKE '%United%';
+FROM world
+WHERE name LIKE '%United%';
 
 -- 7
-SELECT name, population, area
-  FROM world
-  WHERE area > 3000000 OR population > 250000000;
+SELECT name
+   , population
+   , area
+FROM world
+WHERE area > 3000000
+   OR population > 250000000;
 
 -- 8
-SELECT name, population, area
-  FROM world
-  WHERE area > 3000000 XOR population > 250000000;
+SELECT name
+   , population
+   , area
+FROM world
+WHERE area > 3000000
+   XOR population > 250000000;
 
 -- 9
-SELECT name, ROUND(population / 1000000, 2), ROUND(GDP / 1000000000, 2)
-  FROM world
-  WHERE continent = 'South America';
+SELECT name
+   , ROUND(population / 1000000, 2)
+   , ROUND(GDP / 1000000000, 2)
+FROM world
+WHERE continent = 'South America';
 
 -- 10
-SELECT name, ROUND(GDP / population, -3)
-  FROM world
-  WHERE GDP > 1000000000000;
+SELECT name
+   , ROUND(GDP / population, -3)
+FROM world
+WHERE GDP > 1000000000000;
 
 -- 11
-SELECT name, capital
-  FROM world
-  WHERE LENGTH(name) = LENGTH(capital);
+SELECT name
+   , capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital);
 
 -- 12
-SELECT name, capital
-  FROM world
-  WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
+SELECT name
+   , capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1)
+   AND name <> capital;
 
 -- 13
 SELECT name
-  FROM world
-  WHERE name NOT LIKE '% %' AND name LIKE '%a%' AND name LIKE '%e%' AND 
-    name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%';
+FROM world
+WHERE name NOT LIKE '% %'
+   AND name LIKE '%a%'
+   AND name LIKE '%e%'
+   AND name LIKE '%i%'
+   AND name LIKE '%o%'
+   AND name LIKE '%u%';
 
 
 -- -- -- 3 SELECT from Nobel
 --
 
 -- 1
-SELECT yr, subject, winner
-  FROM nobel
-  WHERE yr = 1950;
+SELECT yr
+   , subject
+   , winner
+FROM nobel
+WHERE yr = 1950;
 
 -- 2
 SELECT winner
-  FROM nobel
-  WHERE yr = 1962 AND subject = 'Literature';
+FROM nobel
+WHERE yr = 1962
+   AND subject = 'Literature';
 
 -- 3
-SELECT yr, subject
-  FROM nobel
-  WHERE winner = 'Albert Einstein';
+SELECT yr
+   , subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
 
 -- 4
 SELECT winner
-  FROM nobel
-  WHERE yr >= 2000 AND subject = 'Peace';
+FROM nobel
+WHERE yr >= 2000
+   AND subject = 'Peace';
 
 -- 5
 SELECT *
-  FROM nobel
-  WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+FROM nobel
+WHERE subject = 'Literature'
+   AND yr >= 1980
+   AND yr <= 1989;
 
 -- 6
 SELECT *
-  FROM nobel
-  WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+FROM nobel
+WHERE winner IN (
+   'Theodore Roosevelt'
+   , 'Woodrow Wilson'
+   , 'Jimmy Carter'
+   , 'Barack Obama'
+   );
 
 -- 7
 SELECT winner
-  FROM nobel
-  WHERE winner LIKE 'John_%';
+FROM nobel
+WHERE winner LIKE 'John_%';
 
 -- 8
-SELECT yr, subject, winner
-  FROM nobel
-  WHERE yr = 1980 AND subject = 'Physics' XOR subject = 'Chemistry' AND yr = 1984;
+SELECT yr
+   , subject
+   , winner
+FROM nobel
+WHERE yr = 1980
+   AND subject = 'Physics'
+   XOR subject = 'Chemistry'
+   AND yr = 1984;
 
 -- 9
-SELECT yr, subject, winner
-  FROM nobel
-  WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine';
+SELECT yr
+   , subject
+   , winner
+FROM nobel
+WHERE yr = 1980
+   AND subject <> 'Chemistry'
+   AND subject <> 'Medicine';
 
 -- 10
-SELECT yr, subject, winner
-  FROM nobel
-  WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004;
+SELECT yr
+   , subject
+   , winner
+FROM nobel
+WHERE subject = 'Medicine'
+   AND yr < 1910
+   OR subject = 'Literature'
+   AND yr >= 2004;
 
 -- HARD 11
 SELECT *
-  FROM nobel
-  WHERE winner = 'Peter Grünberg';
+FROM nobel
+WHERE winner = 'Peter Grünberg';
 
 -- HARD 12
 SELECT *
-  FROM nobel
-  WHERE winner = 'Eugene O''Neill';
+FROM nobel
+WHERE winner = 'Eugene O''Neill';
 
 -- HARD 13
-SELECT winner, yr, subject
-  FROM nobel
-  WHERE winner LIKE 'Sir_%'
-  ORDER BY yr DESC;
+SELECT winner
+   , yr
+   , subject
+FROM nobel
+WHERE winner LIKE 'Sir_%'
+ORDER BY yr DESC;
 
 -- HARD 14
-SELECT winner, subject
-  FROM nobel
-  WHERE yr=1984
-  ORDER BY subject IN ('Chemistry', 'Physics') ASC, subject ASC, winner ASC;
+SELECT winner
+   , subject
+FROM nobel
+WHERE yr = 1984
+ORDER BY subject IN (
+      'Chemistry'
+      , 'Physics'
+      ) ASC
+   , subject ASC
+   , winner ASC;
 
 
 -- -- -- 4 SELECT within SELECT
+--
+
+-- 1
+SELECT name
+FROM world
+WHERE population > (
+      SELECT population
+      FROM world
+      WHERE name = 'Russia'
+      );
+
+-- 2
+SELECT name
+FROM world
+WHERE continent = 'Europe'
+   AND gdp / population > (
+      SELECT gdp / population
+      FROM world
+      WHERE name = 'United Kingdom'
+      );
+
+-- 3
+SELECT name
+   , continent
+FROM world
+WHERE continent IN (
+      SELECT continent
+      FROM world
+      WHERE name IN (
+            'Argentina'
+            , 'Australia'
+            )
+      )
+ORDER BY name ASC;
+
+-- 4
+SELECT name
+   , population
+FROM world
+WHERE population > (
+      SELECT population
+      FROM world
+      WHERE name = 'Canada'
+      )
+   AND population < (
+      SELECT population
+      FROM world
+      WHERE name = 'Poland'
+      );
+
+-- 5
+SELECT name
+   , CONCAT (
+      ROUND((
+            population / (
+               SELECT population
+               FROM world
+               WHERE name = 'Germany'
+               ) * 100
+            ), 0)
+      , '%'
+      )
+FROM world
+WHERE continent = 'Europe';
+   
+-- 6
+SELECT name
+FROM world
+WHERE gdp > ALL (
+      SELECT gdp
+      FROM world
+      WHERE gdp > 0
+         AND continent = 'Europe'
+      );
+
+-- 7
+SELECT continent
+   , name
+   , area
+FROM world x
+WHERE area >= ALL (
+      SELECT area
+      FROM world y
+      WHERE y.continent = x.continent
+         AND area > .0
+      );
+
+-- 8
+SELECT continent
+   , name
+FROM world x
+WHERE name <= ALL (
+      SELECT name
+      FROM world y
+      WHERE y.continent = x.continent
+      );
+
+-- DIFFICULT 9
+SELECT name
+   , continent
+   , population
+FROM world x
+WHERE 25000000 >= ALL (
+      SELECT population
+      FROM world y
+      WHERE y.continent = x.continent
+      );
+
+-- DIFFICULT 10
+SELECT name
+   , continent
+FROM world x
+WHERE population / 3 >= ALL (
+      SELECT population
+      FROM world y
+      WHERE y.continent = x.continent
+         AND y.name <> x.name
+      );
+
+
+-- -- -- 5 SUM and COUNT
 --
 
 -- 1
@@ -258,6 +461,3 @@ SELECT winner, subject
 -- 6
 -- 7
 -- 8
--- DIFFICULT 9
--- DIFFICULT 10
-
